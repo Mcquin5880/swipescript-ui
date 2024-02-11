cfb3ceaf23b6e8cda30cd225e877201d1548f0c2
Michael <mquinter88@gmail.com>
2024-02-10 23:15:10 -0500

    skeleton setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HttpClient} from "@angular/common/http";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'flirtmoji-ui';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:8080/api/v1/users').subscribe(data => {
+      this.users = data;
+    });
+  }
 }
